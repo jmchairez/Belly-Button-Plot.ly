bccdfa49d25d63b6f4b981f1ed1066004653692f
Janette Chairez <ChairezFamily@Janettes-iMac-3.local>
2020-11-19 20:44:57 -0600

    added drop down menu

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,7 +1,17 @@
-function init() {
-    d3.json("../data/samples.json").then((data) => {
+d3.json("../data/samples.json").then((data) => {
+        console.log(data)
         
+        var names = data.names;
+        var metadata = data.metadata;
+        var samples = data.samples;
         
+        var message = ("Select ID #");
+        d3.select("#selDataset").append("option").attr("value", message).html(message);
 
-    })
-}
+        var dropdown = d3.select("#selDataset");
+        names.forEach((item) => {
+            var row = dropdown.append("option").attr("value", item);
+            row.text(item);
+        });
+
+    });
