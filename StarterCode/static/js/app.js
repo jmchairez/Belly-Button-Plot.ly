973bf162faecf0a9fa49befe9d07df94624bc924
Janette Chairez <ChairezFamily@Janettes-iMac-3.local>
2020-11-20 19:07:46 -0600

    adding more layout

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,63 +1,66 @@
 d3.json("../data/samples.json").then((data => {
     console.log(data)
         
-    var names = data.names;
-    var metadata = data.metadata;
-    var samples = data.samples;
+var names = data.names;
+var metadata = data.metadata;
+var samples = data.samples;
         
-    var message = ("Select ID #");
-    d3.select("#selDataset").append("option").attr("value", message).html(message);
+var message = ("Select ID #");
+d3.select("#selDataset").append("option")
+.attr("value", message).html(message);
 
-    var dropdown = d3.select("#selDataset");
-    names.forEach((item) => {
-        var row = dropdown.append("option").attr("value", item);
-        row.text(item);
-    });
+var dropdown = d3.select("#selDataset");
+names.forEach((item) => {
+    var row = dropdown.append("option")
+    .attr("value", item);
+    row.text(item);
+});
 
-    const dropdownchange = () => {
+const dropdownchange = () => {
 
-    var demoTable = d3.select("#demographics-table");
-    demoTable.html("")
-    var inputElement = d3.select("#selDataset");
-    var tableBody = demoTable.append("tbody");
-    var inputValue = inputElement.property("value");
+var demoTable = d3.select("#demographics-table");
+demoTable.html("")
+var inputElement = d3.select("#selDataset");
+var tableBody = demoTable.append("tbody");
+var inputValue = inputElement.property("value");
         
-    var filterData = metadata.filter(item => item.id == inputValue);
-    var filterSamples = samples.filter(item => item.id == inputValue);
+var filterData = metadata.filter(item => item.id == inputValue);
+var filterSamples = samples.filter(item => item.id == inputValue);
         
-    var washFreq;
-    filterData.foreach((item) => {
-    let row = tableBody.append("tr");
-    Object.entries(item).forEach(value => {
-        washFreq = item.wfreq;
-        let cell = row.append("tr");
-        cell.text(`${value[0]}: ${value[1]}`);
-    })
-    });
+var washFreq;
+filterData.foreach((item) => {
+let row = tableBody.append("tr");
+Object.entries(item).forEach(value => {
+    washFreq = item.wfreq;
+    let cell = row.append("tr");
+    cell.text("");
+    cell.text(`${value[0]}: ${value[1]}`);
+})
+});
 
-    console.log(washFreq)
+console.log(washFreq)
 
-    var SlicedSampleValues = filteredSamples[0].sample_values.slice(0,10).reverse();
-    var slicedOTUs = filteredSamples[0].otu_ids.slice(0,10).reverse().map(data => `OTU ` + data);
-    var slicedLabels = filteredSamples[0].otu_labels.slice(0,10).reverse();
+var SlicedSampleValues = filteredSamples[0].sample_values.slice(0,10).reverse();
+var slicedOTUs = filteredSamples[0].otu_ids.slice(0,10).reverse().map(data => `OTU ` + data);
+var slicedLabels = filteredSamples[0].otu_labels.slice(0,10).reverse();
 
-    var trace1 = {
-    x: SlicedSampleValues,
-    y: slicedOTUs,
-    text: slicedLabels,
-    type: "bar",
-    orientation: "h",
-    marker: {
-      color: 'purple',
-      width: 1
-    }
-    };
+var trace1 = {
+x: SlicedSampleValues,
+y: slicedOTUs,
+text: slicedLabels,
+type: "bar",
+orientation: "h",
+marker: {
+    color: 'purple',
+    width: 1
+}
+};
 
-    var bardata = [trace1];
-    var barlayout = {
-      title: "Top Microbes in Sample",
-      xaxis: {title: "# Microbes Observed"},
-      yaxis: {title: "OTU ID"},
+var bardata = [trace1];
+var barlayout = {
+    title: "Top Microbes in Sample",
+    xaxis: {title: "# Microbes Observed"},
+    yaxis: {title: "OTU ID"},
 };
 Plotly.newPlot("bar", bardata, barlayout);
 
@@ -75,5 +78,37 @@ var mainPath = path1,
     space = ' ',
     pathY = String(y),
     pathEnd = ' Z';
-
-});
+var path = mainPath.concat(pathX,space,pathY,pathEnd);
+var gaugedata = [{ type: 'scatter',
+    x: [0], y:[0],
+      marker: {size: 14, color:'850000'},
+      showlegend: false,
+      text: level,
+      hoverinfo: 'text'},
+    { values: [81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81/9, 81],
+    rotation: 90,
+    text: ['0-1', '1-2', '2-3', '3-4', '4-5', '5-6', '6-7', '7-8', '8-9', ''],
+    direction: 'clockwise',
+    textinfo: 'text',
+    textposition:'inside',
+    marker: {colors:['maroon', 'red', 'salmon','darkorange', 'orange', 'gold', 'yellow','lightgreen', 'green', 'black'
+    ]},
+    hoverinfo: 'label',
+    hole: .4,
+    type: 'pie',
+    showlegend: false
+}];
+var gaugelayout = {
+  shapes:[{
+    type: 'path',
+    path: path,
+    fillcolor: '850000',
+    line: {color: '850000'}
+    }],
+    title: { text: "Frequency of Bellybutton Washing<br><span style='font-size:0.8em;color:gray;'>Scrubs per Week</span><br><span style='font-size:1em;color:darkgray;font-weight: bold;'></span>"},
+    xaxis: {zeroline:false, showticklabels:false,
+              showgrid: false, range: [-1, 1]},
+    yaxis: {zeroline:false, showticklabels:false,
+              showgrid: false, range: [-1, 1]}
+};
+Plotly.newPlot('gauge', gaugedata, gaugelayout);
